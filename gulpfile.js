b217da15599d00a3b67643f7c39c1da20a23b802
uiliam90@gmail.com <lyoto90>
2018-08-25 15:25:30 +0100

    added sidebar

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var tailwindcss = require('tailwindcss');
 var browserSync = require('browser-sync').create();
 
 gulp.task('css', function () {
-	return gulp.src('./assets/precss/style.css').pipe(
+	return gulp.src('./assets/precss/main.css').pipe(
 		postcss([
-      require('postcss-cssnext')(),
-			require('postcss-responsive-type'),
-			require('postcss-pxtorem')({
-				propList: ['*'],
-				selectorBlackList: ['letter-spacing']
-			})
+			tailwindcss('./tailwind.js'),
+      		// require('postcss-cssnext')(),
+			// require('postcss-responsive-type'),
+			// require('postcss-pxtorem')({
+			// 	propList: ['*'],
+			// 	selectorBlackList: ['letter-spacing']
+			// })
 		]))
 		.pipe(gulp.dest('./assets/css/'))
 		.pipe(browserSync.stream());
@@ -20,7 +22,7 @@ gulp.task('watch', function () {
 	browserSync.init({
 		server: "./"
 	});
-	gulp.watch('./assets/precss/style.css', ['css']);
+	gulp.watch('./assets/precss/main.css', ['css']);
 	gulp.watch('*.html').on('change', browserSync.reload);
 });
 
--- tailwind.js
+++ tailwind.js
@@ -47,86 +47,11 @@ let colors = {
   'transparent': 'transparent',
 
   'black': '#22292f',
-  'grey-darkest': '#3d4852',
-  'grey-darker': '#606f7b',
-  'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
-  'grey-light': '#dae1e7',
-  'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
   'white': '#ffffff',
-
-  'red-darkest': '#3b0d0c',
-  'red-darker': '#621b18',
-  'red-dark': '#cc1f1a',
-  'red': '#e3342f',
-  'red-light': '#ef5753',
-  'red-lighter': '#f9acaa',
-  'red-lightest': '#fcebea',
-
-  'orange-darkest': '#462a16',
-  'orange-darker': '#613b1f',
-  'orange-dark': '#de751f',
-  'orange': '#f6993f',
-  'orange-light': '#faad63',
-  'orange-lighter': '#fcd9b6',
-  'orange-lightest': '#fff5eb',
-
-  'yellow-darkest': '#453411',
-  'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
-  'yellow': '#ffed4a',
-  'yellow-light': '#fff382',
-  'yellow-lighter': '#fff9c2',
-  'yellow-lightest': '#fcfbeb',
-
-  'green-darkest': '#0f2f21',
-  'green-darker': '#1a4731',
-  'green-dark': '#1f9d55',
-  'green': '#38c172',
-  'green-light': '#51d88a',
-  'green-lighter': '#a2f5bf',
-  'green-lightest': '#e3fcec',
-
-  'teal-darkest': '#0d3331',
-  'teal-darker': '#20504f',
-  'teal-dark': '#38a89d',
-  'teal': '#4dc0b5',
-  'teal-light': '#64d5ca',
-  'teal-lighter': '#a0f0ed',
-  'teal-lightest': '#e8fffe',
-
-  'blue-darkest': '#12283a',
-  'blue-darker': '#1c3d5a',
-  'blue-dark': '#2779bd',
-  'blue': '#3490dc',
-  'blue-light': '#6cb2eb',
-  'blue-lighter': '#bcdefa',
-  'blue-lightest': '#eff8ff',
-
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
-  'indigo': '#6574cd',
-  'indigo-light': '#7886d7',
-  'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
-
-  'purple-darkest': '#21183c',
-  'purple-darker': '#382b5f',
-  'purple-dark': '#794acf',
-  'purple': '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff',
-
-  'pink-darkest': '#451225',
-  'pink-darker': '#6f213f',
-  'pink-dark': '#eb5286',
-  'pink': '#f66d9b',
-  'pink-light': '#fa7ea8',
-  'pink-lighter': '#ffbbca',
-  'pink-lightest': '#ffebef',
+  'midnight-black': '#04384A',
+  'teal': '#0B90AA',
+  'canvas': '#EFEBD9',
+  'bean': '#B1B336'
 }
 
 module.exports = {
@@ -226,6 +151,14 @@ module.exports = {
       'Liberation Mono',
       'Courier New',
       'monospace',
+    ],
+    'mont': [
+      'Montserrat',
+      'sans-serif',
+    ],
+    'rob': [
+      'Roboto',
+      'sans-serif',
     ]
   },
 
@@ -250,15 +183,12 @@ module.exports = {
   */
 
   textSizes: {
-    'xs': '.75rem',     // 12px
-    'sm': '.875rem',    // 14px
-    'base': '1rem',     // 16px
-    'lg': '1.125rem',   // 18px
-    'xl': '1.25rem',    // 20px
-    '2xl': '1.5rem',    // 24px
-    '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    'sm': '1rem',       // 16px
+    'base': '1.125rem', // 18px
+    'main': '1.5rem',   // 24px
+    'lg': '3rem',       // 48px
+    'big': '4rem',      // 64px
+    'xbig': '4.5rem'    // 72px
   },
 
 
@@ -469,21 +399,7 @@ module.exports = {
 
   width: {
     'auto': 'auto',
-    'px': '1px',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '5': '1.25rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '24': '6rem',
-    '32': '8rem',
-    '48': '12rem',
-    '64': '16rem',
+    '200': '12.5rem', //200px
     '1/2': '50%',
     '1/3': '33.33333%',
     '2/3': '66.66667%',
@@ -643,21 +559,10 @@ module.exports = {
   */
 
   padding: {
-    'px': '1px',
     '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '5': '1.25rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '20': '5rem',
-    '24': '6rem',
-    '32': '8rem',
+    'px': '1px',
+    '10': '0.625rem',
+    '20': '1.25rem',
   },
 
 
@@ -680,21 +585,9 @@ module.exports = {
     'auto': 'auto',
     'px': '1px',
     '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '5': '1.25rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '20': '5rem',
-    '24': '6rem',
-    '32': '8rem',
+    '10': '0.625rem',
+    '20': '1.25rem',
   },
-
 
   /*
   |-----------------------------------------------------------------------------
