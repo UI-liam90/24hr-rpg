50643cd4d1b20bb9c9535ceeeafbf5d12a574388
uiliam90@gmail.com <lyoto90>
2018-08-26 15:36:01 +0100

    added the new frontpage styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 
 
 var menuToggle = document.querySelector('.open-menu');
-var menu = document.querySelector('.mobile-menu');
-var menuItems = document.querySelectorAll('.mobile-menu > li');
+var menu = document.querySelector('.site-sidebar');
+var offcanvasClose = document.querySelector('.offcanvas-close');
+var menuItems = document.querySelectorAll('.site-sidebar > li');
 menuToggle.addEventListener("click", function( event ) {
     event.preventDefault();
     menu.classList.toggle('active');
-  }, false);
+    offcanvasClose.classList.add('active');
+}, false);
+
+offcanvasClose.addEventListener("click", function( event ) {
+  event.preventDefault();
+  menu.classList.remove('active');
+  offcanvasClose.classList.remove('active');
+}, false);
+
 for (var i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener('click', function(event) {
     menu.classList.remove('active');
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var tailwindcss = require('tailwindcss');
 var browserSync = require('browser-sync').create();
 
 gulp.task('css', function () {
 	return gulp.src('./assets/precss/main.css').pipe(
 		postcss([
-			tailwindcss('./tailwind.js'),
-      		// require('postcss-cssnext')(),
-			// require('postcss-responsive-type'),
-			// require('postcss-pxtorem')({
-			// 	propList: ['*'],
-			// 	selectorBlackList: ['letter-spacing']
-			// })
+      		require('postcss-cssnext')({
+				features: {
+					autoprefixer: false
+				  }
+			  }),
+			require('postcss-responsive-type'),
+			require('postcss-pxtorem')({
+				propList: ['*'],
+				selectorBlackList: ['letter-spacing']
+			}),
+			require('autoprefixer')({ grid: true, browsers: ["last 2 versions", "ie 9-11", "iOS 7"] })
 		]))
 		.pipe(gulp.dest('./assets/css/'))
 		.pipe(browserSync.stream());
