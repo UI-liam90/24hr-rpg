e85547164e5cd663308fd258622e6358113e55a9
uiliam90@gmail.com <lyoto90>
2019-07-20 11:37:05 +0100

    fixing things I hope

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,57 +1,55 @@
-const path = require(`path`)
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+// const path = require(`path`)
+// const { createFilePath } = require('gatsby-source-filesystem')
+// const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+//   return graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             id
+//             frontmatter {
+//               path
+//               templateKey
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors)
+//     }
 
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              path
-              templateKey
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+//     return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: path.resolve(
+//           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`),
+//         context: {
+//           id,
+//         }, // additional data can be passed via context
+//       })
+//     })
+//   })
+// }
 
-    return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`),
-        context: {
-          id,
-        }, // additional data can be passed via context
-      })
-    })
-  })
-}
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
+//   fmImagesToRelative(node) // convert image paths for gatsby images
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ node, getNode })
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value,
+//     })
+//   }
+// }
--- src/templates/adventureTemplate.js
+++ src/templates/adventureTemplate.js
@@ -1,62 +1,59 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
-import Banner from "../components/banner"
-import Layout from "../components/layout"
-import { Helmet } from "react-helmet"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Img from "gatsby-image"
+// import Banner from "../components/banner"
+// import Layout from "../components/layout"
+// import { Helmet } from "react-helmet"
 
-// class Application extends React.Component {
-//   render() {
-//     return (
-//       <div className="application">
-//         <Helmet>
-//           <title>24 hour RPG Challenge 2018 | About The Event</title>
-//         </Helmet>
+// // class Application extends React.Component {
+// //   render() {
+// //     return (
+// //       <div className="application">
+// //         <Helmet>
+// //           <title>24 hour RPG Challenge 2018 | About The Event</title>
+// //         </Helmet>
+// //       </div>
+// //     )
+// //   }
+// // }
+
+// export default function Template({
+//   data, // this prop will be injected by the GraphQL query below.
+// }) {
+//   const { markdownRemark } = data // data.markdownRemark holds our post data
+//   const { frontmatter, html } = markdownRemark
+//   return (
+//     <Layout>
+//       <Helmet>
+//         <title>24 hour RPG Challenge 2019 | {frontmatter.title}</title>
+//       </Helmet>
+//       <Banner src={frontmatter.featuredimage.childImageSharp.fluid} />
+//       <main>
+//         <h3>{frontmatter.gamemode}</h3>
+//       <div className="adventure-content" dangerouslySetInnerHTML={{ __html: html }} />
+//       </main>
+//       <div className="blog-post-container">
 //       </div>
-//     )
-//   }
+//     </Layout>
+//   )
 // }
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <Layout>
-      <Helmet>
-        <title>24 hour RPG Challenge 2019 | {frontmatter.title}</title>
-      </Helmet>
-      <Banner src={frontmatter.featuredimage.childImageSharp.fluid} />
-      <main>
-      <div className="adventure-content" dangerouslySetInnerHTML={{ __html: html }} />
-      </main>
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          
-        </div>
-      </div>
-    </Layout>
-  )
-}
-
-export const pageQuery = graphql`
-  query {
-    markdownRemark {
-      html
-      frontmatter {
-        path
-        title
-        gamemode
-        featuredimage {
-          childImageSharp {
-            fluid(maxWidth: 500) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query {
+//     markdownRemark {
+//       html
+//       frontmatter {
+//         path
+//         title
+//         gamemode
+//         featuredimage {
+//           childImageSharp {
+//             fluid(maxWidth: 500) {
+//               ...GatsbyImageSharpFluid_withWebp
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
