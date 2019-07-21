32e70aab3f1bcb215172cc3ccd1fa5411d13300f
UI-liam90 <uiliam90@gmail.com>
2019-07-21 17:17:14 +0100

    Update adventureTemplate.js

--- src/templates/adventureTemplate.js
+++ src/templates/adventureTemplate.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Banner from "../components/banner"
 import Layout from "../components/layout"
+import Container from "../components/Container"
 import { Helmet } from "react-helmet"
 
 // class Application extends React.Component {
