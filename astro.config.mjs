c1c293f0af59b3811e1cb30fbbe932112a1e2050
Liam Wright <liam.wright@mosaiconline.co.uk>
2022-06-23 20:50:20 +0100

    added header commponent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [astroImageTools],
+    placeholder: "tracedSVG",
+    format: ["webp", "jpg"],
+    fallbackFormat: "png",
+    includeSourceFormat: false,
+    formatOptions: {
+        jpg: {
+        quality: 80,
+        },
+        png: {
+        quality: 80,
+        },
+        webp: {
+        quality: 50,
+        },
+        tracedSVG: {
+            options: {
+                background: "#fff",
+                color: "#000",
+                turnPolicy: "black",
+                turdSize: 1,
+                alphaMax: 1,
+                optCurve: true,
+                threshold: 100,
+                blackOnWhite: false,
+            },
+        },
+    },
+});
---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-nested'),
+        require('postcss-custom-media'),
+        require('autoprefixer'),
+        require('cssnano'),
+    ],
+  };
